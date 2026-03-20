20b444da52da3218f18766bad65415217833e958
Douglas R <douglascorreia17@gmail.com>
2026-03-20 09:12:40 -0300

    fix: Corrigido geração de cores nas classes do tailwind na página de detalhe do pokémon.

--- src/components/details/PokemonDetailType.jsx
+++ src/components/details/PokemonDetailType.jsx
@@ -4,7 +4,12 @@ function PokemonDetailType({types}) {
             <ul className="flex flex-wrap items-center gap-3">
                 { types && types.map((type, index) => {
                     return(
-                        <li key={index} className={`leading-none py-2 px-4 rounded-xs text-${type.type.name}-600 font-semibold bg-${type.type.name}-300`}>{type.type.name}</li>
+                        <li
+                            key={index}
+                            className={`leading-none py-2 px-4 rounded-xs font-semibold background-type-${type.type.name}`}
+                        >
+                            {type.type.name}
+                        </li>
                     );
                 })}
             </ul>
